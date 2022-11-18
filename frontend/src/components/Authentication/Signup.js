7103660a91eff6700561c931bd52b3e518779f37
Alvin Chan <alvinchan315@gmail.com>
2022-11-18 15:33:08 -0500

    material UI signup page

--- frontend/src/components/Authentication/Signup.js
+++ frontend/src/components/Authentication/Signup.js
@@ -1,36 +1,25 @@
-import { Button } from "@chakra-ui/button";
-import { FormControl, FormLabel } from "@chakra-ui/form-control";
-import { Input, InputGroup, InputRightElement } from "@chakra-ui/input";
-import { VStack } from "@chakra-ui/layout";
 import { useToast } from "@chakra-ui/toast";
 import axios from "axios";
 import { useState } from "react";
-//import { useHistory } from "react-router";
-import { Navigate, useNavigate } from 'react-router-dom';
+import { Navigate, useNavigate } from "react-router-dom";
+import { TextField, Button, Box } from "@material-ui/core";
 const Signup = () => {
-  const [show, setShow] = useState(false);
-  const handleClick = () => setShow(!show);
   const toast = useToast();
-  //const history = useHistory();
-    const navigate = useNavigate()
+  const navigate = useNavigate();
   const [name, setName] = useState();
   const [username, setUsername] = useState();
   const [confirmpassword, setConfirmpassword] = useState();
   const [password, setPassword] = useState();
-  const [pic, setPic] = useState();
-  const [picLoading, setPicLoading] = useState(false);
 
   const submitHandler = async () => {
-    setPicLoading(true);
     if (!name || !username || !password || !confirmpassword) {
       toast({
-        title: "Please Fill all the Feilds",
+        title: "Please Fill All the Fields",
         status: "warning",
         duration: 5000,
         isClosable: true,
         position: "bottom",
       });
-      setPicLoading(false);
       return;
     }
     if (password !== confirmpassword) {
@@ -43,7 +32,6 @@ const Signup = () => {
       });
       return;
     }
-    console.log(name, username, password, pic);
     try {
       const config = {
         headers: {
@@ -56,11 +44,9 @@ const Signup = () => {
           name,
           username,
           password,
-          pic,
         },
         config
       );
-      console.log(data);
       toast({
         title: "Registration Successful",
         status: "success",
@@ -69,7 +55,6 @@ const Signup = () => {
         position: "bottom",
       });
       localStorage.setItem("userInfo", JSON.stringify(data));
-      setPicLoading(false);
       navigate("/blog");
     } catch (error) {
       toast({
@@ -80,121 +65,60 @@ const Signup = () => {
         isClosable: true,
         position: "bottom",
       });
-      setPicLoading(false);
-    }
-  };
-
-  const postDetails = (pics) => {
-    setPicLoading(true);
-    if (pics === undefined) {
-      toast({
-        title: "Please Select an Image!",
-        status: "warning",
-        duration: 5000,
-        isClosable: true,
-        position: "bottom",
-      });
-      return;
-    }
-    console.log(pics);
-    if (pics.type === "image/jpeg" || pics.type === "image/png") {
-      const data = new FormData();
-      data.append("file", pics);
-      data.append("upload_preset", "chat-app");
-      data.append("cloud_name", "piyushproj");
-      fetch("https://api.cloudinary.com/v1_1/piyushproj/image/upload", {
-        method: "post",
-        body: data,
-      })
-        .then((res) => res.json())
-        .then((data) => {
-          setPic(data.url.toString());
-          console.log(data.url.toString());
-          setPicLoading(false);
-        })
-        .catch((err) => {
-          console.log(err);
-          setPicLoading(false);
-        });
-    } else {
-      toast({
-        title: "Please Select an Image!",
-        status: "warning",
-        duration: 5000,
-        isClosable: true,
-        position: "bottom",
-      });
-      setPicLoading(false);
-      return;
     }
   };
 
   return (
-    <VStack spacing="5px">
-      <FormControl id="first-name" isRequired>
-        <FormLabel>Name</FormLabel>
-        <Input
-          placeholder="Enter Your Name"
-          onChange={(e) => setName(e.target.value)}
-        />
-      </FormControl>
-      <FormControl id="username" isRequired>
-        <FormLabel>username</FormLabel>
-        <Input
-          type="username"
-          placeholder="username"
-          onChange={(e) => setUsername(e.target.value)}
-        />
-      </FormControl>
-      <FormControl id="password" isRequired>
-        <FormLabel>Password</FormLabel>
-        <InputGroup size="md">
-          <Input
-            type={show ? "text" : "password"}
-            placeholder="Enter Password"
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          <InputRightElement width="4.5rem">
-            <Button h="1.75rem" size="sm" onClick={handleClick}>
-              {show ? "Hide" : "Show"}
-            </Button>
-          </InputRightElement>
-        </InputGroup>
-      </FormControl>
-      <FormControl id="password" isRequired>
-        <FormLabel>Confirm Password</FormLabel>
-        <InputGroup size="md">
-          <Input
-            type={show ? "text" : "password"}
-            placeholder="Confirm password"
-            onChange={(e) => setConfirmpassword(e.target.value)}
-          />
-          <InputRightElement width="4.5rem">
-            <Button h="1.75rem" size="sm" onClick={handleClick}>
-              {show ? "Hide" : "Show"}
-            </Button>
-          </InputRightElement>
-        </InputGroup>
-      </FormControl>
-      <FormControl id="pic">
-        <FormLabel>Upload your Picture</FormLabel>
-        <Input
-          type="file"
-          p={1.5}
-          accept="image/*"
-          onChange={(e) => postDetails(e.target.files[0])}
-        />
-      </FormControl>
-      <Button
-        colorScheme="blue"
-        width="100%"
-        style={{ marginTop: 15 }}
-        onClick={submitHandler}
-        isLoading={picLoading}
-      >
-        Sign Up
-      </Button>
-    </VStack>
+    <div>
+      <TextField
+        margin="normal"
+        fullWidth
+        required
+        id="outlined-basic"
+        label="Username"
+        variant="standard"
+        onChange={(e) => setName(e.target.value)}
+      />
+      <TextField
+        margin="normal"
+        fullWidth
+        required
+        id="outlined-basic"
+        label="Name"
+        variant="standard"
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <TextField
+        margin="normal"
+        fullWidth
+        required
+        id="outlined-basic"
+        label="Password"
+        variant="standard"
+        type="password"
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <TextField
+        margin="normal"
+        fullWidth
+        required
+        id="outlined-basic"
+        label="Confirm Password"
+        variant="standard"
+        type="password"
+        onChange={(e) => setConfirmpassword(e.target.value)}
+      />
+      <Box m={2} pt={3}>
+        <Button
+          variant="contained"
+          color="primary"
+          fullWidth
+          onClick={submitHandler}
+        >
+          Sign Up
+        </Button>
+      </Box>
+    </div>
   );
 };
 

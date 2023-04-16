3a69c45b74af2d86ba265ed3ce885ee50d75284a
Nicolas D Michel <nicolas.michel@uconn.edu>
2023-04-16 11:00:19 -0400

    changes to enc

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -3,6 +3,7 @@ import asyncHandler from "express-async-handler"
 import User from '../models/userModel.js'
 // const User = require("../models/userModel");
 import generateToken from '../config/generateToken.js';
+import crypto from 'crypto'
 // const generateToken = require("../config/generateToken");
 
 //@description     Get or Search all users
@@ -27,7 +28,79 @@ const allUsers = asyncHandler(async (req, res) => {
 //@access          Public
 const registerUser = asyncHandler(async (req, res) => {
   const { name, username, password, pic } = req.body;
+  console.log("before b64 decode\n")
+  console.log(password)
+  //const encPass = Buffer.from(password, 'base64');
+  // stored privkey 
+  const privKey = "-----BEGIN PRIVATE KEY-----\n"+
+  "MIIJQgIBADANBgkqhkiG9w0BAQEFAASCCSwwggkoAgEAAoICAQDHkESNdQu+aI/s\n"+
+  "31cUdOyT3mvQp7iXuxo2hHqpRX914m9qfOM9wbB66oUDkzrXspcZK5OHDEosxyeR\n"+
+  "nPh0OpDk1Aqe8axyghN1ybKOyoUCQBXyb+hZfUZepansa+VEr6cW2vTIj0qJyVaX\n"+
+  "KOeAnPOlh7/drbn7NLSwJnaprpwAi/zxcgHdrg0s17xWdi5HYXqNNT0xvWmuQyZz\n"+
+  "jnUqz1QC5DEvYNJfVjfmHKSf6wxH3F6jxONlNqA41VpazrhaglX+xTenkvXOOOdX\n"+
+  "9W8EAGxWRwFaScGDqA56NBcO6VHxwbzInx+dt90126bu05QezznO4vpBs8cfuNfJ\n"+
+  "HxGfwR6oQrTyNnn4p1rLi0sl66fyGlqpnjmz4eGimijv+lKkTWhYZuGFBFh8/wRL\n"+
+  "G/97MMNUByqMApZxk1RCwdsKfxdJ0dwJ/KYeOMdqw8wWFZghsLVPiyfthhm2AUZJ\n"+
+  "yfQxIfIMF5vj+DbNa62lyJlJPK/IOYRykBnZh1eBsRltSXJQzumewW2+/HSt3Iet\n"+
+  "GF5tz+tYnbX5MaFnsqoNdIgtgXHFIodf7F4ikp4SxG4U+61qTtwHV7nWhIivHssJ\n"+
+  "IxbiWHlm2fSxyZ0gs4mNO7EVKpTFcgiPZXTZLZpntd5OCD6jbR7jtUGD7Y4JYa2A\n"+
+  "gvIAGciPzdcp328QBrUXC5EjHDlVCQIDAQABAoICAARxeAfwQbUubc9Cf8ADBHDH\n"+
+  "DykjpYQB/0jpyoix9URiKMSQXZzhOZ/a+zWDqy058Db4Fej4Az9w+wwObdJkqsI1\n"+
+  "iGZWj4Dd2DdoqL2Y0RJQ6/bXEATMucA0ubj7rqyd/pSPMSEKs7L9VRy4z+1jUTdv\n"+
+  "yNFTjg8DnZGdzEmQi4T67kRP8K5csuAQjkRx4tY7BjgA+0j8DyTOoPC0t3N0ZJ9L\n"+
+  "MQnS2Dbq3AQXF4E/V1QoRMEqX+e/1McUf6utlkQ0Slw0TD4XvbpRi7EVKVtySPc4\n"+
+  "zdUSoajf7oaWWfxpLHu7VQvgeHCSCJkA4tFxdjGjaoWVNH45qvwlU5n07kxbcpEW\n"+
+  "w5VuAmskexabp8KAGcMLUbSvS8w+fZi/8Bh9iPDPniNNO8mUEMx63gp23FCK9zfh\n"+
+  "rE8T/3/g4s6QjTUBA0wNROzNzB0DdN85Ohd4LRBX3kB3GBmDjnBQ00mQHWnvEUvM\n"+
+  "0+FSUINtXonsdUZgWLIGx/mGWnMXMUEb7B2+mRIYu87Wta9TZ8UjYCqRUdXjCrJl\n"+
+  "UpeiGTldo/ky5aAIPImj05jt2T0Y+YG0ZC9eWBvpD0LrlkjO0rdtKPQYo6P+kBSn\n"+
+  "+z5/wX9bKGhRZ0G+jwAKFHyeW5UkBZidFiCq+KxmV1lqLz1/oom3CsXOKCj57iH5\n"+
+  "kZniS802RKPHJt/8mD/pAoIBAQDlEeGtI1fXOW4I7R5ZXksg8tJCnADLtBF/pu0p\n"+
+  "pfymnKAZceKKLIo7KR4dejOAffw7YRC/+9mhg/1o1b9aakKSa/ycyPI+fKUPIeqw\n"+
+  "dOv4GSfHvpUwsU7VeSQSd4+t0NSivBllfbqpxnQniuhwLdeXt0HkZN2bYMtLMWEw\n"+
+  "GPGflf6UQm45OonZ2qdfGX5blMo0lCwfSFtjW4hTGt9AwDRbkSs6FEAi4iJZU31H\n"+
+  "rTVuCTkpwTW6TvkloHP8HDyt+go5q3evEdHceAkIarSBqFwytIsdEDD8HGkslXtS\n"+
+  "iw+7B7tp/g/d0TtZoBq/ysrH+xlNIrdVA07tUci3TZdE21CVAoIBAQDfBlxAu1W2\n"+
+  "RUHGm8IP6sVXQLeDxCTIBFqm3B0FWTrO860vNw7ltdaTz3rZe4fZ838dPyjOiPQ3\n"+
+  "3s/ibqqTH2fRPAzHpxw0cTEeI12rv1I2lyA/DtOzbJPc1YeaBD0/+6L7LFELg9Ay\n"+
+  "kozFqNrqNAlYJQh+zFPpGJuVNQmL+YvIGG4sFUqMPP+jl06Bb+HwCil4ImxAmh1D\n"+
+  "hZQnVc8NRtSUkW19p9fuU7HcmviSUdwTHbkQuV7GgfMMi2lGfOtH8a7H5SPwfz3E\n"+
+  "BBHuwF8678GvhOawgyexSi+KWnCPvvlcsaXvKH2lHthW24LH++E8yfEDmdjQAoQ8\n"+
+  "0veUgevI0pGlAoIBAQCGHRwF5GVvav5TV5CnPh+6YLBdszx8Bycfl7nO4pGzcowB\n"+
+  "mb3KrB0aWg55r6wrlJdypHfMD0Xx3HcnQ83Ov9vOIk859CRwFUYGQBNJ0Iq/LOsv\n"+
+  "dROf+I9xSIWoEjwaxeNYi8xKsXvyHnCvHhLMauqItxBrCer9J4Set4BxSolcp9Lv\n"+
+  "Np2WAAHpfKz1btWeBNUGR6I1hoWK/HIqRddj76Q34Bxw2p5aTg9SQSFeL7jSfa/s\n"+
+  "FGTCea5BK1n8xwN30Kbf8NYXt9UowVZ4iRC5wLqKLU4ta+diE7g4FCSpxSUmmXZO\n"+
+  "riLX+cJHzQccNzzWLNs5YBP6P/+bzoA3YcQo2kapAoIBABAEoe2YRuCverjsCPUh\n"+
+  "tIi+wi95j/a5sVUYrBDtRrcEc2TXvH8tHztAhJ9IKwqxXWjZV3H2DixRGXc24ZNt\n"+
+  "hwRVHsncK79sfc2fVqP2Os5m7k75HqVdYAVVZMbgFRIaXatSlIOSNQrc0bWEmiyG\n"+
+  "DkHBbynEPSaZuNrvkz71bl+xuX/7C36lButV+7J6Oa0G3dGeD05Yg0+NMYXduOBI\n"+
+  "PQ5qn4WN34ljg3vHuaCwtnC9CsSzu6XgQXeeL7J7WF6zcg0zXPByAxqn/C28rVpL\n"+
+  "TAryLatx0BEAtgdm1XzNh5VrwPI7Y0LPw1QBshPw+E1oZ6INk6/k7mB7+oI1Dp0+\n"+
+  "2m0CggEAdledI0sQ9Ps0S1PFoPBu0i44pfGRTLpoKzZVFH+lNENVTgNgqvhploF2\n"+
+  "/gzbIZb7lC3eSV9T+jE8OicamLS4y0UdZah2mKvD+Ouno8ZhfickRdUJ6S+/Pq4w\n"+
+  "Cz3PVf/mtc6KVbMJRgymj7sTACbVJUOOnibVnO4Pww/+KSXlbojHyAx9bEw6rJj9\n"+
+  "sKtxTq/2iRTJakajoQFds5s9O23jkt9kFQ5dcXMiz/XefJxULvCP785N/0P4nZ56\n"+
+  "TYXDUpMOfjzeNQ/nTL6RR0+MgYePaxHxXn/o7mG3B3E8cCHyc+5MIGh68U7v3Njb\n"+
+  "biP3dhkPuQVq1LgLo87+QJ/6beIRRQ==\n"+
+  "-----END PRIVATE KEY-----\n"
 
+  //decrypt password
+  console.log("arrived before decryption")
+  //console.log(encPass)
+  /*const decPass = crypto.privateDecrypt(
+    {
+      key: privKey,
+      // In order to decrypt the data, we need to specify the
+      // same hashing function and padding scheme that we used to
+      // encrypt the data in the previous step
+      padding: crypto.constants.RSA_PKCS1_OAEP_PADDING,
+      oaepHash: "sha256",
+    },
+    encPass
+  );
+  console.log("decrypted data: ", decPass.toString());*/
+  
   if (!name || !username || !password) {
     res.status(400);
     throw new Error("Please Enter all the Feilds");
--- frontend/src/components/Authentication/Signup.js
+++ frontend/src/components/Authentication/Signup.js
@@ -3,6 +3,9 @@ import axios from "axios";
 import { useState } from "react";
 import { Navigate, useNavigate } from "react-router-dom";
 import { TextField, Button, Box } from "@material-ui/core";
+import fs from 'fs'
+import crypto from 'crypto'
+
 const Signup = () => {
   const toast = useToast();
   const navigate = useNavigate();
@@ -33,6 +36,42 @@ const Signup = () => {
       return;
     }
     try {
+      //encrypt password before sending 
+
+/*console.log("before pubkey import")
+      const publicKey = Buffer.from(
+        fs.readFileSync("./pubK.pem", { encoding: "utf-8" })
+      );
+      console.log("Passed pubkey import") */
+
+      const publicKey = "-----BEGIN PUBLIC KEY-----\n" + 
+      "MIICIjANBgkqhkiG9w0BAQEFAAOCAg8AMIICCgKCAgEAx5BEjXULvmiP7N9XFHTs\n"+
+      "k95r0Ke4l7saNoR6qUV/deJvanzjPcGweuqFA5M617KXGSuThwxKLMcnkZz4dDqQ\n"+
+      "5NQKnvGscoITdcmyjsqFAkAV8m/oWX1GXqWp7GvlRK+nFtr0yI9KiclWlyjngJzz\n"+
+      "pYe/3a25+zS0sCZ2qa6cAIv88XIB3a4NLNe8VnYuR2F6jTU9Mb1prkMmc451Ks9U\n"+
+      "AuQxL2DSX1Y35hykn+sMR9xeo8TjZTagONVaWs64WoJV/sU3p5L1zjjnV/VvBABs\n"+
+      "VkcBWknBg6gOejQXDulR8cG8yJ8fnbfdNdum7tOUHs85zuL6QbPHH7jXyR8Rn8Ee\n"+
+      "qEK08jZ5+Kday4tLJeun8hpaqZ45s+Hhopoo7/pSpE1oWGbhhQRYfP8ESxv/ezDD\n"+
+      "VAcqjAKWcZNUQsHbCn8XSdHcCfymHjjHasPMFhWYIbC1T4sn7YYZtgFGScn0MSHy\n"+
+      "DBeb4/g2zWutpciZSTyvyDmEcpAZ2YdXgbEZbUlyUM7pnsFtvvx0rdyHrRhebc/r\n"+
+      "WJ21+TGhZ7KqDXSILYFxxSKHX+xeIpKeEsRuFPutak7cB1e51oSIrx7LCSMW4lh5\n"+
+      "Ztn0scmdILOJjTuxFSqUxXIIj2V02S2aZ7XeTgg+o20e47VBg+2OCWGtgILyABnI\n"+
+      "j83XKd9vEAa1FwuRIxw5VQkCAwEAAQ==\n"+
+      "-----END PUBLIC KEY-----\n"
+
+      const encryptedData = crypto.publicEncrypt(
+        {
+          key: publicKey,
+          padding: crypto.constants.RSA_PKCS1_OAEP_PADDING,
+          oaepHash: "sha256",
+        },
+        // We convert the data string to a buffer using `Buffer.from`
+        Buffer.from(password)
+      );
+      console.log("encypted data: ", encryptedData.toString("base64"));
+      const b64dat = encryptedData.toString("base64")
+      console.log("Passed encryption")
+
       const config = {
         headers: {
           "Content-type": "application/json",
